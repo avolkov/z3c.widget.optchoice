2dedfbf7b87d098f2d18bb57c378340614c64173
Alex Volkov <alex.volkov@caseware.com>
2012-11-27 17:05:58 -0500

    Set up proper test cases for OptChoiceWidget. TODO: render proper page.

--- z3cwidgetoptchoice/tests.py
+++ z3cwidgetoptchoice/tests.py
@@ -1,13 +1,33 @@
 import unittest2 as unittest
 from z3c.form.testing import TestRequest
+from widget import OptChoiceWidget
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.component.interfaces import ComponentLookupError
 
 class TestBasicOptChoice(unittest.TestCase):
     def setUp(self):
         self.request = TestRequest()
-    def test_init(self):
-        from widget import OptChoice
-        self.assertEqual(OptChoice.__name__, 'OptChoice')
-    def test_custom_template(self):
-        from widget import OptChoice
-        oc = OptChoice(self.request)
-        self.assertEquals(oc.template.template(oc), u'<h1>Hello, world!</h1>\n')
+    def test_import(self):
+        self.assertEqual(OptChoiceWidget.__name__, 'OptChoiceWidget')
+    def test_custom_template_noterms(self):
+        """Sequence widget with no terms raises ComponentLookupError"""
+        oc = OptChoiceWidget(self.request)
+        with self.assertRaises(ComponentLookupError):
+            oc.update()
+            data = oc.render()
+
+    def test_custom_template_terms(self):
+        oc = OptChoiceWidget(self.request)
+        oc.terms = SimpleVocabulary([
+                        SimpleTerm(value="first", title="First"),
+                        SimpleTerm(value="second", title="Second")
+                                    ])
+        oc.update()
+        data = oc.render()
+        import pdb; pdb.set_trace()
+        self.assertGreaterEqual(len(data), 28)
+    def test_init_widget(self):
+        opt_widget = OptChoiceWidget(self.request)
+        opt_widget.name = 'opt-choice'
+        self.assertIsNone(opt_widget.terms)
--- z3cwidgetoptchoice/widget.py
+++ z3cwidgetoptchoice/widget.py
@@ -1,14 +1,19 @@
 import os.path
 import zope.interface
 import zope.schema
+import zope.component
 
 from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 from z3c.form.widget import Widget, FieldWidget
 from z3c.form import interfaces
 
 class OptChoiceWidget(Widget):
-    zope.interface.implements(interfaces.IWidget)
+    zope.interface.implements(interfaces.ISequenceWidget)
+    value = ()
+    terms = None
     klass = u'optchoice-widget'
+
+    noValueToken ='--NOVALUE--'
 
     def __init__(self, request):
         dirname = os.path.dirname(os.path.abspath(__file__))
@@ -16,17 +21,27 @@ class OptChoiceWidget(Widget):
         self.template = ViewPageTemplateFile(outp)
         super(self.__class__, self).__init__(request)
 
-    @property
-    def items(self):
-        if self.terms is None:
-            return ()
-
     def render(self):
         template = self.template
         return template(self)
 
+    def updateTerms(self):
+        if self.terms is None:
+            self.terms = zope.component.getMultiAdapter(
+                (self.context, self.request, self.form, self.field, self),
+                interfaces.ITerms)
+        return self.terms
     def update(self):
         """This is where all the interesting stuff happens"""
+        self.updateTerms()
+        super(self.__class__, self).update()
+    def extract(self, default=interfaces.NO_VALUE):
+        #Get value from the request
+        #TODO: check that value exists
+        value = self.request.get(self.name, default)
+        #TODO: add more or fewer checks compared to SequenceWidget
+        return value
+
 
 @zope.component.adapter(zope.schema.interfaces.IField, interfaces.IFormLayer)
 def OptChoiceWidgetFactory(field, request):
